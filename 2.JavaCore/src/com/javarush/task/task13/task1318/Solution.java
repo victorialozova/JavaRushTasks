48c022d05444fae94946b9c2c5a515b334671dcf
victorialozova <hunter4ik@gmail.com>
2017-07-21 11:19:34 +0300

    Чтение файла 1. Считать с консоли имя файла. 2. Вывести в консоль(на экран) содержимое файла. 3. Не забыть освободить ресурсы. Закрыть поток чтения с файла и поток ввода с клавиатуры.
    Требования:
    1. Программа должна считывать c консоли имя файла.
    2. Программа должна выводить на экран содержимое файла.
    3. Поток чтения из файла (FileInputStream) должен быть закрыт.
    4. BufferedReader также должен быть закрыт.

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task13.task1318;
+
+import java.io.*;
+import java.util.Scanner;
+
+/* 
+Чтение файла
+1. Считать с консоли имя файла.
+2. Вывести в консоль(на экран) содержимое файла.
+3. Не забыть освободить ресурсы. Закрыть поток чтения с файла и поток ввода с клавиатуры.
+
+Требования:
+1. Программа должна считывать c консоли имя файла.
+2. Программа должна выводить на экран содержимое файла.
+3. Поток чтения из файла (FileInputStream) должен быть закрыт.
+4. BufferedReader также должен быть закрыт.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream file = new FileInputStream(reader.readLine());
+        while (file.available() > 0) {
+            System.out.print((char)file.read());
+        }
+        System.out.println("");
+        file.close();
+        reader.close();
+    }
+}
