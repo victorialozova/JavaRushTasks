73dca5f0686d70c99da066d7842b952b8dacb65f
victorialozova <hunter4ik@gmail.com>
2017-09-11 12:43:42 +0300

    DownloadException 1 Считывать с консоли имена файлов. 2 Если файл меньше 1000 байт, то: 2.1 Закрыть потоки работы с файлами. 2.2 Выбросить исключение DownloadException.
    Требования:
    1. Программа должна считать имена файлов с консоли.
    2. Для чтения из файлов используй поток FileInputStream.
    3. Программа должна работать, пока введенный файл не окажется меньше 1000 байт.
    4. Программа должна завершиться исключением DownloadException.
    5. Поток FileInputStream должен быть закрыт.

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -0,0 +1,41 @@
+package com.javarush.task.task18.task1810;
+
+/* 
+DownloadException
+1 Считывать с консоли имена файлов.
+2 Если файл меньше 1000 байт, то:
+2.1 Закрыть потоки работы с файлами.
+2.2 Выбросить исключение DownloadException.
+
+Требования:
+1. Программа должна считать имена файлов с консоли.
+2. Для чтения из файлов используй поток FileInputStream.
+3. Программа должна работать, пока введенный файл не окажется меньше 1000 байт.
+4. Программа должна завершиться исключением DownloadException.
+5. Поток FileInputStream должен быть закрыт.
+*/
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws DownloadException, IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            String name = reader.readLine();
+            FileInputStream file = new FileInputStream(name);
+            if (file.available() < 1000) {
+                file.close();
+                reader.close();
+                throw new DownloadException();
+            }
+            file.close();
+        }
+    }
+
+    public static class DownloadException extends Exception {
+
+    }
+}
