18f00dbef08170f5ceb6583480d8e498fd5fc91d
victorialozova <hunter4ik@gmail.com>
2017-09-12 20:17:01 +0300

    Считаем слово Считать с консоли имя файла. Вывести в консоль количество слов «world«, которые встречаются в файле. Закрыть потоки.

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task19.task1907;
+
+/* 
+Считаем слово
+Считать с консоли имя файла.
+Файл содержит слова, разделенные знаками препинания.
+Вывести в консоль количество слов «world«, которые встречаются в файле.
+Закрыть потоки.
+
+Требования:
+1. Программа должна считывать имя файла с консоли (используй BufferedReader).
+2. BufferedReader для считывания данных с консоли должен быть закрыт.
+3. Программа должна считывать содержимое файла (используй FileReader c конструктором String).
+4. Поток чтения из файла (FileReader) должен быть закрыт.
+5. Программа должна выводить в консоль количество слов "world", которые встречаются в файле.
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file = reader.readLine();
+        FileReader fr = new FileReader(file);
+        char[] arr = new char[50];
+        String str = "";
+        int count = 0;
+        while (fr.ready()) {
+            fr.read(arr, 0, 50);
+            str+=String.valueOf(arr);
+        }
+        String s = "world";
+        String[] w = str.toLowerCase().split(s);
+        count = w.length - 1;
+        reader.close();
+        fr.close();
+        System.out.println(count);
+    }
+}
