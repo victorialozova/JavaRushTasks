f5c4d187b7db3a9e333b1ca7b2cc4b281f9dda63
victorialozova <hunter4ik@gmail.com>
2017-09-13 12:22:37 +0300

    Ридер обертка 2 В методе main подмените объект System.out написанной тобой ридер-оберткой по аналогии с лекцией. Твоя ридер-обертка должна заменять все подстроки «te» на «??«. Вызови готовый метод printSomething(), воспользуйтесь testString. Верни переменной System.out первоначальный поток. Выведи модифицированную строку в консоль.

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1912/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task19.task1912;
+
+/* 
+Ридер обертка 2
+В методе main подмените объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна заменять все подстроки «te» на «??«.
+Вызови готовый метод printSomething(), воспользуйтесь testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+
+
+Требования:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу
+проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "it's a text for testing".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c
+параметром конструктора BytqeArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль
+объекта System.out
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething()
+согласно заданию, и выводить её в консоль.
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+        //String[] res1 = result.re.split("te");
+        String res = result.replace("te", "??");
+        System.out.println(res);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
