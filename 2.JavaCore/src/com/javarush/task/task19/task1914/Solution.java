f567ac9d3f7e447caa3985a9e232cf6d125754c6
victorialozova <hunter4ik@gmail.com>
2017-09-13 14:44:39 +0300

    ешаем пример В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией. Твоя ридер-обертка должна выводить на консоль решенный пример. Вызови готовый метод printSomething(), воспользуйтесь testString. Верни переменной System.out первоначальный поток.
    Возможные операции: + - *
    Шаблон входных данных и вывода: a [знак] b = c
    Отрицательных и дробных чисел, унарных операторов — нет.
    
    Пример вывода:
    3 + 6 = 9

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1914/Solution.java
@@ -0,0 +1,66 @@
+package com.javarush.task.task19.task1914;
+
+/* 
+Решаем пример
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна выводить на консоль решенный пример.
+Вызови готовый метод printSomething(), воспользуйтесь testString.
+Верни переменной System.out первоначальный поток.
+
+Возможные операции: + - *
+Шаблон входных данных и вывода: a [знак] b = c
+Отрицательных и дробных чисел, унарных операторов — нет.
+
+Пример вывода:
+3 + 6 = 9
+
+Требования:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу
+проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "3 + 6 = ".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream
+c параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль
+объекта System.out.
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething()
+согласно заданию, и выводить её в консоль.
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+        testString.printSomething();
+        String result = outputStream.toString();
+        System.setOut(consoleStream);
+        String[] res1 = result.split(" ");
+        int a = Integer.parseInt(res1[0]);
+        int b = Integer.parseInt(res1[2]);
+        int sum = 0;
+        if (res1[1].equals("+")){
+            sum = a + b;
+        } else if (res1[1].equals("-")) {
+            sum = a - b;
+        } else if (res1[1].equals("*")) {
+            sum = a * b;
+        }
+        System.out.print(outputStream.toString() + sum);
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("3 + 6 = ");
+        }
+    }
+}
+
